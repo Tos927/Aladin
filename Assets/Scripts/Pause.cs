ff49ef85c5d4564c09ae1267dac5c92d169430c5
Tos927 <t.sautereau92@gmail.com>
2022-04-28 22:02:31 +0200

    fuck this

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    /*public GameObject pauseScreen;
+
+    void Update()
+    {
+    }
+
+    void SetPause()
+    {
+        TimeScale.time = 0;
+        pauseScreen.SetActive(true);
+    }
+
+    void SetPlay()
+    {
+        TimeScale.time = 1;
+        pauseScreen.SetActive(false);
+    }*/
+}
