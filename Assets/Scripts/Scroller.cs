1857b5e06aa15283c285dce6f9214b63fbbed811
Tos927 <t.sautereau92@gmail.com>
2022-04-28 08:38:13 +0200

    Background operationnel

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -1,15 +1,15 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
-    [SerializeField] private RawImage img;
-    [SerializeField] private float x, y;
+    public float speed;
+    [SerializeField] private Renderer bgRenderer; 
 
     void Update()
     {
-        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+        bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
     }
 }
